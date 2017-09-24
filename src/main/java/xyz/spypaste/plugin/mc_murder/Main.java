eeabbba87d98098615e6f42a9e86e9f990b7a2e8
Spypaste <spypaste@outlook.jp>
2017-09-24 16:08:33 +0900

    Update main class (add method)

--- src/main/java/xyz/spypaste/plugin/mc_murder/Main.java
+++ src/main/java/xyz/spypaste/plugin/mc_murder/Main.java
@@ -1,17 +1,31 @@
 package xyz.spypaste.plugin.mc_murder;
 
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
-
+    private  static Main instance = null;
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        instance = this;
+        registerListener();
+        registerCommand();
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public static Main getInstance() {
+        return instance;
+    }
+
+    private void registerListener(){
+        PluginManager pluginManager = getServer().getPluginManager();
+    }
+
+    private void registerCommand(){
     }
 }
